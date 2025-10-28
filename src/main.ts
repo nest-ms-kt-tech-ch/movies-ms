25c3f2be2071f02f93a087c1c0f9ce2b7102f668
Erick Sanchez <sanchezjoserick@gmail.com>
2025-10-28 17:17:12 -0600

    [ :hammer: ] feat: add host to ms options

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,{
     transport: Transport.TCP,
     options: {
+      host: '0.0.0.0',
       port: envs.PORT,
     },
   });
